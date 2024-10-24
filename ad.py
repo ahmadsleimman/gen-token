e66f8399d89a01fa93ac35e42198bbf28f0425d5
ahmadsleimman <ahmadtsleiman@outlook.com>
2024-10-24 15:49:54 +0300

    add deploy

--- ad.py
+++ ad.py
@@ -1,1 +1,10 @@
-print("safa")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return 'Hello, Flask with Nginx!'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)  # Running on port 5000
